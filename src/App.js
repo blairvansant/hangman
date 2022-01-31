71732e508d286af4e874bb9a4aac1bdb02638ae7
gitblairshub <blairsarlo@gmail.com>
2022-01-31 18:59:15 -0500

    Added Figure component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import Figure from './components/Figure';
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <Figure />
     </div>
   );
 }
