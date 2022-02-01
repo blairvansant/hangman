7a0e750ff736dc57f50637925c44d772e7623998
gitblairshub <blairsarlo@gmail.com>
2022-02-01 11:03:59 -0500

    Added Notification and Popup components - both placeholder code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from './components/Header';
 import Figure from './components/Figure';
 import WrongLetters from './components/WrongLetters';
 import Word from './components/Word';
+import Popup from './components/Popup';
+import Notification from './components/Notification';
 
 
 function App() {
@@ -13,6 +15,8 @@ function App() {
         <Figure />
         <WrongLetters />    
         <Word />
+        <Popup />
+        <Notification />
       </div>
     </div>
   );
