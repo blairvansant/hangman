98a5f2b361dc90e37a392891599c14da5513d0f4
gitblairshub <blairsarlo@gmail.com>
2022-02-01 23:47:58 -0500

    Added functionality for checking for win/lose and their associated notifications

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { checkWin } from '../helpers/helpers';
 
-const Popup = () => {
+const Popup = ({ correctLetters, wrongLetters, selectedWord, setPlayable }) => {
+  let finalMessage = '';
+  let finalMessageRevealWord = '';
+  let playable = true;
+
+  if( checkWin(correctLetters, wrongLetters, selectedWord) === 'win' ) {
+    finalMessage = 'Congratulations! You won! 😃';
+    playable = false;
+  } else if ( checkWin(correctLetters, wrongLetters, selectedWord) === 'lose') {
+    finalMessage = 'Unfortunately you lost. 🙁';
+    finalMessageRevealWord = `...the word was: ${selectedWord}`;
+  }
+
   return (
-    <div className='popup-container' id='popup-container'>
+    <div className='popup-container'>
       <div className='popup'>
-        <h2 id='final-message'></h2>
-        <h3 id='final-message-reveal-word'></h3>
-        <button id='play-button'>Play Again</button>
+        <h2>{finalMessage}</h2>
+        <h3>{finalMessageRevealWord}</h3>
+        <button>Play Again</button>
       </div>
     </div>
   )
